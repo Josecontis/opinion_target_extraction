ff655349e1299850615daf80e45db8daf40f8540
Josecontis <peppeco98@gmail.com>
2021-01-14 15:50:50 +0100

    Refactoring propagation.py file to solve a big mistake
    mistake solved: error to write on opinion file the words caused by an ambiguous variable name

--- ProgettoTesi/src/propagation.py
+++ ProgettoTesi/src/propagation.py
@@ -1,4 +1,5 @@
 from src import rule
+import csv
 import nltk
 from nltk.tokenize import sent_tokenize
 
@@ -13,58 +14,54 @@ def sentence_tokenize(input):
 
 def propagation(sentences, local_corenlp_path): # questo metodo prende in input la lista di frasi e la libreria
 
-    #sentences = sentence_tokenize(input)
-
     for input in sentences: # per ogni singola frase nella lista di frasi
 
-        target = open('../target.txt', 'a') # apre il file dei target inizialmente vuoto in append
-        opinion = open('../opinion.txt', 'a') # apre il file delle parole di opinione inizialmente vuoto in append
+        with open('../target.txt', 'a') as file_tar_r1: # apre il file dei target inizialmente vuoto in append
 
-        target.write('\n') # ritorno a capo per l'append di nuove frasi
-        target.write(input +'#') # inserimento di # dopo la frase
-        opinion.write('\n') # ritorno a capo per l'append di nuove frasi
-        opinion.write(input + '#') # inserimento di # dopo la frase
+            print("REGOLA 1")
+            print('\n')
+            tar_r1 = rule.R1(local_corenlp_path, input) # estrapolazione del target tramite la regola R1, passando
+                                                        # al metodo la libreria e la frase da esaminare
+            for word_r1 in tar_r1:    # targ è la lista di tutti i target estratti dalla singola frase (ovvero input)
+                file_tar_r1.write('\n') # scrive uno spazio nel file dopo #
+                file_tar_r1.write(word_r1)  # scrive nel file i target trovati
+                file_tar_r1.write(', ') # scrive uno spazio nel file dopo il target per separlo con un altro alla prossima iterata
 
-        print("REGOLA 1")
-        print('\n')
-        targ = rule.R1(local_corenlp_path, input) # estrapolazione del target tramite la regola R1, passando
-                                                    # al metodo la libreria e la frase da esaminare
-        for element in targ:    # targ è la lista di tutti i target estratti dalla singola frase (ovvero input)
-            target.write(' ')   # scrive uno spazio nel file dopo #
-            target.write(element)  # scrive nel file i target trovati
-            target.write(' ')  # scrive uno spazio nel file dopo il target per separlo con un altro alla prossima iterata
+        with open('../opinion.txt', 'a') as file_opi_r2:
+            target = '../target.txt' # la variabile viene aggiornata con il file dei target singoli estratti prima
+            print("REGOLA 2")
+            print('\n')
+            opi_r2 = rule.R2(local_corenlp_path, input, target)# estrapolazione della parola di opinione tramite la regola R2, passando
+                                                        # al metodo la libreria e la frase da esaminare, e il file dei target singoli estratti prima
+            for word_r2 in opi_r2: # opin è la lista di tutte le parola di opinione estratte dalla singola frase (ovvero input)
+                file_opi_r2.write('\n') # scrive uno spazio nel file dopo #
+                file_opi_r2.write(word_r2) # scrive nel file la parola di opinione trovate
+                file_opi_r2.write(', ') # scrive uno spazio nel file dopo la parola di opinione per separlo con un altra alla prossima iterata
 
-        target = '../lessico_target_opinion/Lessico-Target.txt' # la variabile viene aggiornata con il file dei target singoli estratti prima
-        print("REGOLA 2")
-        print('\n')
-        opin = rule.R2(local_corenlp_path, input, target)# estrapolazione della parola di opinione tramite la regola R2, passando
-                                                    # al metodo la libreria e la frase da esaminare, e il file dei target singoli estratti prima
-        for element in opin: # opin è la lista di tutte le parola di opinione estratte dalla singola frase (ovvero input)
-            opinion.write(' ') # scrive uno spazio nel file dopo #
-            opinion.write(element) # scrive nel file la parola di opinione trovate
-            opinion.write(' ') # scrive uno spazio nel file dopo la parola di opinione per separlo con un altra alla prossima iterata
+        with open('../target.txt', 'a') as file_tar_r3:  # apre il file dei target inizialmente vuoto in append
+            targe = '../target.txt'
+            print("REGOLA 3")
+            print('\n')
+            tar_r3 = rule.R3(local_corenlp_path, input, targe)# estrapolazione del target tramite la regola R3, passando
+                                                        # al metodo la libreria, la frase da esaminare e il file dei target singoli estratti prima
+            print(tar_r3)
+            for word_r3 in tar_r3:
+                file_tar_r3.write('\n') # scrive uno spazio nel file dopo #
+                file_tar_r3.write(word_r3) # scrive nel file i target trovati
+                file_tar_r3.write(', ') # scrive uno spazio nel file dopo il target per separlo con un altro alla prossima iterata
 
-        target = open('../target.txt', 'a') # apre il file dei target ora non vuoto in append
-        targe = '../lessico_target_opinion/Lessico-Target.txt' # la variabile targe è il file dei target singoli estratti prima
-        print("REGOLA 3")
-        print('\n')
-        targ = rule.R3(local_corenlp_path, input, targe)# estrapolazione del target tramite la regola R3, passando
-                                                    # al metodo la libreria, la frase da esaminare e il file dei target singoli estratti prima
-        for element in targ:
-            target.write(' ') # scrive uno spazio nel file dopo #
-            target.write(element) # scrive nel file i target trovati
-            target.write(' ') # scrive uno spazio nel file dopo il target per separlo con un altro alla prossima iterata
+        with open('../opinion.txt', 'a') as file_opi_r4:
+            opin = '../opinion.txt' # la variabile opin è il file delle parole di opinione singole estratti prima
+            print("REGOLA 4")
+            print('\n')
+            opi_r4 = rule.R4(local_corenlp_path, input, opin) # estrapolazione della parola di opinione tramite la regola R4, passando
+                                                        # al metodo la libreria, la frase da esaminare e il file dei target singoli estratti prima
+            for word_r4 in opi_r4:
+                file_opi_r4.write('\n')  # scrive uno spazio nel file dopo #
+                file_opi_r4.write(word_r4)  # scrive nel file la parola di opinione trovate
+                file_opi_r4.write(', ')  # scrive uno spazio nel file dopo la parola di opinione per separlo con un altra alla prossima iterata
 
-        opinion = open('../opinion.txt', 'a') # apre il file delle parole di opinione ora non vuoto in append
-        opin = '../lessico_target_opinion/Lessico-Opinion.txt'# la variabile opin è il file delle parole di opinione singole estratti prima
-        print("REGOLA 4")
-        print('\n')
-        op = rule.R4(local_corenlp_path, input, opin) # estrapolazione della parola di opinione tramite la regola R4, passando
-                                                    # al metodo la libreria, la frase da esaminare e il file dei target singoli estratti prima
-        for element in op:
-            opinion.write(' ') # scrive uno spazio nel file dopo #
-            opinion.write(element) # scrive nel file la parola di opinione trovate
-            opinion.write(' ') # scrive uno spazio nel file dopo la parola di opinione per separlo con un altra alla prossima iterata
-
-        target.close() # chiusura del file dei target estratti
-        opinion.close() # chiusura del file delle parole di opinione estratte
+        file_tar_r1.close()  # chiusura del file dei target estratti
+        file_tar_r3.close()  # chiusura del file dei target estratti
+        file_opi_r2.close()  # chiusura del file delle parole di opinione estratte
+        file_opi_r4.close()  # chiusura del file delle parole di opinione estratte
