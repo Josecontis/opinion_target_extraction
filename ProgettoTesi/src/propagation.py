7d7e41598fc9d0b766252e19a80fde228c1dd6cd
Josecontis <peppeco98@gmail.com>
2021-01-20 15:00:01 +0100

    Propagation write error to file modified
    changed '\n' charachter position, to correct writing on files of targets and opinion words

--- ProgettoTesi/src/propagation.py
+++ ProgettoTesi/src/propagation.py
@@ -12,7 +12,7 @@ def propagation(sentences, local_corenlp_path):  # questo metodo prende in input
             tar_r1 = rule.R1(local_corenlp_path, phrase)  # estrapolazione del target tramite la regola R1, passando
                                                           # al metodo la libreria e la frase da esaminare
 
-            # file_tar_r1.write('\n')  # ritorna a capo per scrivere nuovi target
+            file_tar_r1.write('\n')  # ritorna a capo per scrivere nuovi target
             for word_r1 in tar_r1:  # targ è la lista di tutti i target estratti dalla singola frase (ovvero input)
                 file_tar_r1.write(word_r1)  # scrive nel file i target trovati
                 file_tar_r1.write(', ')  # scrive uno spazio nel file dopo il target per separlo con un altro alla prossima iterata
@@ -24,7 +24,7 @@ def propagation(sentences, local_corenlp_path):  # questo metodo prende in input
             opi_r2 = rule.R2(local_corenlp_path, phrase, target)  # estrapolazione della parola di opinione tramite la regola R2, passando
                                                         # al metodo la libreria e la frase da esaminare, e il file dei target singoli estratti prima
 
-            # file_opi_r2.write('\n')  # ritorna a capo per scrivere nuove parole di opinione
+            file_opi_r2.write('\n')  # ritorna a capo per scrivere nuove parole di opinione
             for word_r2 in opi_r2:  # opin è la lista di tutte le parola di opinione estratte dalla singola frase (ovvero input)
                 file_opi_r2.write(word_r2)  # scrive nel file la parola di opinione trovate
                 file_opi_r2.write(', ')  # scrive uno spazio nel file dopo la parola di opinione per separlo con un altra alla prossima iterata
@@ -36,7 +36,7 @@ def propagation(sentences, local_corenlp_path):  # questo metodo prende in input
             tar_r3 = rule.R3(local_corenlp_path, phrase, targe)  # estrapolazione del target tramite la regola R3, passando
                                                         # al metodo la libreria, la frase da esaminare e il file dei target singoli estratti prima
 
-            file_tar_r3.write('\n')  # ritorna a capo per scrivere nuovi target
+            #file_tar_r3.write('\n')  # ritorna a capo per scrivere nuovi target
             for word_r3 in tar_r3:
                 file_tar_r3.write(word_r3)  # scrive nel file i target trovati
                 file_tar_r3.write(', ')  # scrive uno spazio nel file dopo il target per separlo con un altro alla prossima iterata
@@ -48,7 +48,6 @@ def propagation(sentences, local_corenlp_path):  # questo metodo prende in input
             opi_r4 = rule.R4(local_corenlp_path, phrase, opin)  # estrapolazione della parola di opinione tramite la regola R4, passando
                                                                 # al metodo la libreria, la frase da esaminare e il file dei target singoli estratti prima
 
-            file_opi_r4.write('\n')  # ritorna a capo per scrivere nuove parole di opinione
             for word_r4 in opi_r4:
                 file_opi_r4.write(word_r4)  # scrive nel file la parola di opinione trovate
                 file_opi_r4.write(', ')  # scrive uno spazio nel file dopo la parola di opinione per separlo con un altra alla prossima iterata
